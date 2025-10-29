ed3c52f6c9aa0b9680cdd11124b3bc662472450a
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-10-29 12:01:59 +0530

    first commit

---
+++ src/Header/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css'
+
+function Header(){
+    return (
+        <div className='mynav'>
+            <img src='https://edupoly.in/assets/fullstack-training-hyderabad.png'/>
+            <ul>
+                <li>Home</li>
+                <li>AboutUs</li>
+                <li>ContactUs</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/Mainarea/Mainarea.jsx
@@ -0,0 +1,15 @@
+function MainArea(){
+    return (
+        <div style={{display:"flex",alignItems:"center"}}>
+            <div style={{fontSize:"1.5em",display:"flex",flexDirection:"column",width:"50%",alignItems:"center"}}>
+                <h1>Code Your Career</h1>
+                <h2>Prepare Practice Perform</h2>
+            </div>
+            <div style={{width:"50%"}}>
+                <img style={{width:"750px"}} src="https://edupoly.in/assets/best-fullstack-training-hyderabad.png"/>
+            </div>
+        </div>
+    )
+}
+
+export default MainArea;
